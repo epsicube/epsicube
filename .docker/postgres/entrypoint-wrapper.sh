3958d60a5958b993621817e494fe1a7b8822ebd0
COLANT Alan <alan.colant@uni-deal.com>
2026-03-24 14:09:29 +0100

    feat: add retention configuration for backup

--- .docker/postgres/entrypoint-wrapper.sh
+++ .docker/postgres/entrypoint-wrapper.sh
@@ -4,7 +4,6 @@ set -Eeuo pipefail
 # --- Global Variables ---
 CUSTOM_CONFIG="/usr/local/pgbackrest.conf"
 CRON_TMP="/tmp/crontab"
-CURRENT_PGDATA="${PGDATA:-/var/lib/postgresql/data}"
 
 # --- Functions ---
 
@@ -29,19 +28,28 @@ handle_pgbackrest_conf() {
         echo "Config: No custom configuration found. Generating default $PGBACKREST_CONFIG"
         cat <<EOF > "$PGBACKREST_CONFIG"
 [main]
-pg1-path=${CURRENT_PGDATA}
+pg1-path=${PGDATA:-/var/lib/postgresql/data}
 pg1-port=5432
 
 [global]
 repo1-path=/var/lib/pgbackrest
 spool-path=/spool/pgbackrest
 lock-path=/spool/pgbackrest
+
 repo1-bundle=y
 repo1-bundle-limit=20MiB
+
 archive-async=y
 archive-copy=y
-repo1-retention-full=30
+
+start-fast=y
+delta=y
+
+repo1-retention-full=${BACKUP_FULL_RETENTION:-52}
+repo1-retention-diff=${BACKUP_DIFF_RETENTION:-31}
 repo1-retention-archive-type=full
+repo1-retention-archive=${BACKUP_ARCHIVE_RETENTION:-31}
+
 process-max=2
 compress-type=zst
 compress-level=3
@@ -76,6 +84,7 @@ initialize_pgbackrest_async() {
 
 schedule_backups() {
     [ -n "${BACKUP_FULL_CRON:-}" ] && echo "${BACKUP_FULL_CRON} pgbackrest --stanza=main --type=full backup" >> "$CRON_TMP"
+    [ -n "${BACKUP_DIFF_CRON:-}" ] && echo "${BACKUP_DIFF_CRON} pgbackrest --stanza=main --type=diff backup" >> "$CRON_TMP"
     [ -n "${BACKUP_INCR_CRON:-}" ] && echo "${BACKUP_INCR_CRON} pgbackrest --stanza=main --type=incr backup" >> "$CRON_TMP"
 }
 
